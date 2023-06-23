59a70fae8578f3a33f42c810195f3dc12d86c35d
Arman777-coder <armanasatryan914@gmail.com>
2023-06-23 21:48:26 +0400

    site

---
+++ index.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="img/attachment_119176701.jpg">
+<!--    <link rel="stylesheet" href="bootstrap-4.0.0">-->
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="code.jquery.com_jquery-3.7.0.min.js"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>-->
+    <title>Vsyodlyatortov</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div id="img_div">
+        <img src="img/Untitled-1.jpg" alt="Icon" style="height: 67px">
+
+    </div>
+    <div id="nav">
+        <div class="nav_elem"><h4>Home</h4></div>
+        <div class="nav_elem"><h4>About</h4></div>
+        <div class="nav_elem"><h4>Blog</h4></div>
+        <div class="nav_elem"><h4>Cakes</h4></div>
+        <div class="nav_elem"><h4>Elements</h4></div>
+    </div>
+    <div id="forms">
+        <i class="fa fa-phone" style="font-size:24px;margin-top: 25px;margin-right: 5px"></i>
+        <h5 id="nav_inp">+7 915 999 76 54</h5>
+        <a href="#"> <i class="fa fa-instagram" style="font-size:24px;margin-top: 20px;cursor: pointer"></i></a>
+
+
+    </div>
+</nav>
+
+<div class="slideshow-container">
+
+    <div class="mySlides fade">
+
+        <img src="img/wdy050121lifeoftheparty-003-1621611282.jpg" style="width:100%;height: 580px;border-bottom: 3px solid  #bb0012">
+
+    </div>
+
+    <div class="mySlides fade">
+
+        <img src="img/Toy-Names-For-Kids.jpg" style="width:100%;height: 580px;border-bottom: 3px solid  #bb0012">
+
+    </div>
+
+    <div class="mySlides fade">
+
+        <img src="img/asp-prazdnik-1_-103-oci6hpbgqn8r1zbwk7jyg0aqnmvrjmj057fbtedpmw-ocjnj0xf5kymb1xlgiwe973u642oy4elzh3vljeqko.jpeg" style="width:100%;height: 580px;border-bottom: 3px solid  #bb0012">
+
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1)">❮</a>
+    <a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center;display: none">
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+    <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+<div id="header">
+    <h1 id="header_text">Our Sales</h1>
+</div>
+<div id="glob_sale">
+<div id="opt">
+    <div class="opt_sel"><h4 class="opt_h4">Home</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4">Toys</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4">Plays</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4">Cakes</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4">Bisquites</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4">More</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4">Igri</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4">Stolnie</h4></div>
+    <div class="opt_sel"><h4 class="opt_h4"></h4></div>
+    <div class="opt_sel"><h4 class="opt_h4"></h4></div>
+    <div class="opt_sel"><h4 class="opt_h4"></h4></div>
+</div>
+<div id="sale">
+<div id="shop">
+    <div id="shop_text_div">
+    <h1 id="shop_text">Top Sale</h1>
+    </div>
+    <div id="shop_id">
+    <div id="shop_img_div"><img src="img/download.jpg" alt="" id="shop_img"></div>
+       <div id="name_div"> <div id="name">Cake</div> </div>
+        <div id="gin"><h5 id="gin_h5">2300р</h5></div>
+        <div id="option"><p id="option_p"> A sweet baked food made from a dough<br> or thick batter</p>
+
+            </div>
+    </div>
+</div>
+    <div id="tes_div">
+    <div id="tes">
+        <h3 class="chose" id="chose1">Cakes</h3>
+        <h3 class="chose">Toys</h3>
+        <h3 class="chose">Biscuites</h3>
+    </div>
+    </div>
+<div id="where">
+    <div>Text</div>
+    <div><img src="" alt=""></div>
+</div>
+    <div id="where1">
+        <div>Text</div>
+        <div><img src="" alt=""></div>
+    </div>
+    <div id="where2">
+        <div>Text</div>
+        <div><img src="" alt=""></div>
+    </div>
+</div>
+</div>
+<script src="js/script.js"></script>
+</body>
+</html>
